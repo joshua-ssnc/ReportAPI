f22fbd3bc2b07184874d40f4c7f6c68990a7fb57
Joshua McInerney <joshua@ssnc.co.kr>
2025-04-03 17:23:39 +0900

    Gitignore and main updated

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import sys
+
 import logging as log
 
 from fastapi import FastAPI,Depends,Query # type: ignore
 from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-from typing  import Annotated
+from typing  import Annotated, Union
 from pydantic import BaseModel # type: ignore
-from util import database, models, crud, schemas
+from Report import database, crud
 
 app = FastAPI()
 log_level = log.INFO
@@ -19,7 +19,7 @@ def generate_firewall_report(db: Session = Depends(database.get_db)):
     return StreamingResponse(crud.generate_firewall_report(db), media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", headers={"Content-Disposition": "attachment; filename=firewall_report.xlsx"})
 
 @app.get("/firewalls/generate-report-data")
-def generate_report_data(db: Session = Depends(database.get_db), fw_ids: Annotated[list[int] | None, Query()] = None):
+def generate_report_data(db: Session = Depends(database.get_db), fw_ids: Annotated[Union[list[int], None], Query()] = None):
     report_data = crud.generate_report_data(db=db, firewall_id=-1, fw_ids=fw_ids)
 
     return report_data
